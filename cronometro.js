82ceeb73fe8d0f91ccf47589d3020404888c58b4
hiltonjfn <88454528+hiltonjfn@users.noreply.github.com>
2023-06-12 03:56:50 -0300

    Add files via upload

--- cronometro.js
+++ cronometro.js
@@ -2,10 +2,12 @@ var timer; // Variável global para armazenar o intervalo
 var countdown; // Variável para armazenar o tempo restante
 
 function startTimer() {
-    try{  var timeElement = document.querySelector("#baliza p");
-    timeElement.remove();}catch{}
+  try {
+    var timeElement = document.querySelector("#baliza p");
+    timeElement.remove();
+  } catch { }
 
-  
+
   countdown = 300; // 5 minutos em segundos
   var baliza = document.getElementById("baliza");
 
@@ -24,32 +26,32 @@ function startTimer() {
   var timeElement = document.createElement("p");
   timeElement.id = "time-element"; // Adiciona um ID ao elemento para manipulação posterior
   timeElement.textContent = formatTime(countdown);
-  baliza.appendChild(timeElement,timeElement,timeElement);
+  baliza.appendChild(timeElement, timeElement, timeElement);
 
 
   //checkbox!
-var div = document.createElement('div');
-div.id = 'checkbox_div';
+  var div = document.createElement('div');
+  div.id = 'checkbox_div';
 
-// Define o estilo da div
-div.style.display = 'inline-block';
+  // Define o estilo da div
+  div.style.display = 'inline-block';
 
-// Cria os três inputs
-for (var i = 0; i < 3; i++) {
-  var input = document.createElement('input');
-  input.type = 'checkbox';
-  input.className = 'form-check-input col-2';
-  
-  // Adiciona os inputs à div
-  div.appendChild(input);
-}
-baliza.appendChild(div);
+  // Cria os três inputs
+  for (var i = 0; i < 3; i++) {
+    var input = document.createElement('input');
+    input.type = 'checkbox';
+    input.className = 'form-check-input col-2';
+
+    // Adiciona os inputs à div
+    div.appendChild(input);
+  }
+  baliza.appendChild(div);
 
 
 
 
   // Cria um intervalo para atualizar o tempo a cada segundo
-  timer = setInterval(function() {
+  timer = setInterval(function () {
     countdown--;
 
     // Atualiza o texto do elemento <p> com o tempo restante formatado
@@ -63,7 +65,7 @@ baliza.appendChild(div);
 }
 
 function stopTimer() {
-  
+
 
   clearInterval(timer);
   var baliza = document.getElementById("baliza");
@@ -75,7 +77,7 @@ function stopTimer() {
   var checkbox_div = document.getElementById('checkbox_div')
   checkbox_div.remove()
 
-    // Remove o antigo 'p'
+  // Remove o antigo 'p'
   var timeElement = document.getElementById('time-element');
   timeElement.remove();
 
@@ -103,3 +105,33 @@ function formatTime(seconds) {
   var formattedSeconds = secondsRemaining.toString().padStart(2, "0");
   return formattedMinutes + ":" + formattedSeconds;
 }
+
+
+var indice = [0, 0, 0, 0, 0]
+
+
+const recurso1 = () => alterarImagem(["img/img1.jpg", "img/img2.jpg", "img/img3.jpg", "img/img4.jpg", "img/img5.jpg", "img/img6.jpg"], 1)
+
+const recurso2 = () => alterarImagem(["img/img8.jpg", "img/img9.jpg", "img/img10.jpg"], 2)
+
+const recurso3 = () => alterarImagem(["img/img11.jpg", "img/img12.jpg", "img/img13.jpg", "img/img13.jpg"], 3)
+
+const recurso4 = () => alterarImagem(["img/img15.jpg", "img/img16.jpg", "img/img17.jpg", "img/img18.jpg", "img/img19.jpg"], 4)
+
+function chamarFuncoes() {
+  recurso1();
+  recurso2();
+  recurso3();
+  recurso4();
+}
+
+function alterarImagem(imagens, x) {
+  var imagem1 = document.getElementById(`image${x}`);
+  indice[x] = (indice[x] + 1) % imagens.length; // Alterna entre as imagens em loop
+  imagem1.src = imagens[indice[x]];
+
+
+
+}
+
+setInterval(chamarFuncoes, 1000); // Chama a função a cada 5 segundos (5000 milissegundos)
